02a3255b6c02cf6038e60c451371b70c8eedd3a2
deepred5 <948931744@qq.com>
2023-02-10 19:59:38 +0800

    feat: 0.0.5

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,2 +1,2 @@
 export { default as useDynamicRem } from './dynamic-rem';
-export { default as useForceLandscape } from './force-landscape';
+export { default as useForceLandscape } from './force-landscape';package.json
